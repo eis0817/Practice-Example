1cc67243f027243dd25e743c440cb0f3a89e4d90
[eis0817] <[eis0817@gmail.com]>
2025-12-05 14:14:14 +0900

    숫자형

---
+++ 10. 프로그래밍_핵심개념/1-1. 숫자형.js
@@ -0,0 +1,24 @@
+const a = 10;
+const b = 3.141592;
+
+//덧셈
+console.log(10 + 10);
+//뺄셈
+console.log(4 - 1);
+//곱셈
+console.log(10*10);
+//나눗셈
+console.log(9/3);
+//나머지 연산
+console.log(10 % 2);
+
+function isEven(n) {
+  console.log(n % 2 === 0);
+}
+
+isEven(3); //false
+
+//승 표시
+const num1 = 2 ** 10;
+const num2 = 3 ** 4;
+console.log(num2);
