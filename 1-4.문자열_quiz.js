f4583a41e85a39d251d9e14ef623114c7c07ac66
[eis0817] <[eis0817@gmail.com]>
2025-12-05 14:43:41 +0900

    문자열 퀴즈

---
+++ 1-4.문자열_quiz.js
@@ -0,0 +1,22 @@
+// greeting 변수에 문자열 "Hello, World!"가 할당되도록
+
+let greeting = 'Hello, ' + 'World!';
+console.log(greeting);
+
+
+//
+let num1 = 20;
+let num2 = 24;
+
+let str1 = '20';
+let str2 = '24';
+
+console.log(num1 + num2); // 44
+console.log(str1 + str2); // 2024
+
+//`sentence` 변수에 할당하고 출력해보세요.
+
+// **출력할 문장:** `He said, "I'm a programmer."`
+
+let sentence = 'He said, "I\'m a programmer.\"';
+console.log(sentence);
---
+++ 10. 프로그래밍_핵심개념/1_3.문자열_기본.js
@@ -0,0 +1,14 @@
+const text = 'I love javascript'; // string
+const someText = "hello world"; // string
+
+/*
+따옴표는 앞뒤 한가지로 통일해서 적어야한다
+큰 따옴표로 감싸진 경우, 그 안에 따옴표로 감싸야 한다면 서로 다른 종류를 쓰면 됨
+'그가 말했다. \'안녕\'' << 이런 경우,
+\' 이걸 쓰면 된다
+*/
+
+console.log('hello ' + 'world');
+
+const name = '지은서';
+const message = '안녕';
