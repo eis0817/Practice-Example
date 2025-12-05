2705cfa8e16f4ef748ba1dc61a0a17ab90de0deb
[eis0817] <[eis0817@gmail.com]>
2025-12-05 14:19:16 +0900

    숫자형 퀴즈

---
+++ 1-1. quiz.js
@@ -0,0 +1,13 @@
+// ### 문제 2
+
+// 세 명의 친구가 저녁 식사 비용으로 총 54,000원이 나왔습니다.
+//  이 비용을 똑같이 나누어 내려고 합니다. 
+// let을 사용하여 totalCost와 numberOfFriends 변수를 선언하고
+//  각각 54,000과 3을 할당하세요. 
+// 그 후, 한 사람당 내야 할 금액을 계산하여 costPerPerson 변수에 저장하고, 
+// 이 변수를 콘솔에 출력해보세요.
+
+let totalCost = 54000;
+let numberofFriends = 3;
+let costPerPerson = totalCost / numberofFriends
+console.log(costPerPerson); //18000
