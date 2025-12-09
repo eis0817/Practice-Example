d12b8a81232cc8965e8ce94251bc2468e0ffa0a7
eis0817 <eis0817@gmail.com>
2025-12-09 14:23:00 +0900

    객체와프로퍼티

---
+++ 11.프로그래밍과_데이터/1-1.객체와_프로퍼티.js
@@ -0,0 +1,25 @@
+// 객체, key(property)-value
+const myComputer = {
+  manufacturer: 'Apple',
+  modelName: 'MacBook M1 Pro',
+  releaseYear: 2001,
+  isLaptop: true,
+  mainboard: {
+    cpu: 'M1',
+    ram: '16GB',
+  },
+};
+
+const data = {
+  'serial-number': 'ABC-123-XYZ',
+  'last-update-date': '2025-11-12',
+};
+
+// 변수 표기 방법
+// serialNumber // camel case
+// SerialNumber // pascal case
+// serial-number // kebab case
+// serial_number // snake case
+
+console.log('myComputer', myComputer);
+console.log(typeof myComputer); // object
